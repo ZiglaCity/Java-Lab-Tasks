37e4f4155a17ef996c367e7da6541dfe60f3f6e4
ZiglaCity <ziglacity@gmail.com>
2025-01-21 21:43:47 +0000

    Finished with abstraction

--- Encapsulation/CommissionEmployee/FullTimeEmployee.java
+++ Encapsulation/CommissionEmployee/FullTimeEmployee.java
@@ -5,6 +5,9 @@ public class FullTimeEmployee extends Employee {
 
     public FullTimeEmployee(String name, String employeeId, double salary){
         super(name, employeeId);
+        if(salary <= 0){
+            throw new IllegalArgumentException("Salary must be greater than 0");
+        }
         this.salary = salary;
     }
 
@@ -22,6 +25,9 @@ public class FullTimeEmployee extends Employee {
         System.out.println("Employee Name: " + employee1.getName());
         System.out.println("Employee ID: " + employee1.getEmployeeId());
         System.out.println("Employee Salary: " + employee1.getSalary());
+        double pay = employee1.calculatePay();
+        System.out.println("Employee Salary: " + pay);
 
     }
+    
 }
